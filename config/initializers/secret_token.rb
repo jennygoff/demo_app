5e6274af418127cab82ed785fbb12c383fb1b2b9
Jenny Goff <jennyg@effectivephilanthropy.org>
2013-04-03 16:13:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f54f1300fc09545501ca60d8b764cdf5ef1257e8bb8c77a7703611841ec0ee911d838fd13042ec9e34f2e36606c5ea640179816bb6ba1a8d7eed87428a95dee8'
